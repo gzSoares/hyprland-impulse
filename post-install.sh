3dd501f528687a4d9c39f2235839321e4628f898
Geazi Soares <soares.geazi@hotmail.com>
2026-05-16 07:21:38 +0000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -e
 
 # Troca remote Fedora pelo Flathub
@@ -8,11 +9,26 @@ if flatpak remotes | grep -q '^fedora'; then
     flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 fi
 
+# Instala Flatpaks do Flathub
+flatpak install -y flathub \
+    org.mozilla.firefox \
+    org.remmina.Remmina \
+    com.discordapp.Discord \
+    com.spotify.Client \
+    com.thincast.client \
+    im.riot.Riot \
+    org.fedoraproject.MediaWriter \
+    org.gtk.Gtk3theme.adw-gtk3-dark \
+    page.codeberg.JakobDev.jdReplace \
+    page.codeberg.libre_menu_editor.LibreMenuEditor \
+    com.dec05eba.gpu_screen_recorder \
+    org.gnome.TextEditor
+
 # Copia dotfiles do skel pro home de todos os usuários que ainda não os têm
 for userdir in /var/home/*/; do
     user=$(basename "$userdir")
     if [ "$user" = "*" ]; then continue; fi
-    for dotfile in /etc/skel/.*  /etc/skel/*; do
+    for dotfile in /etc/skel/.* /etc/skel/*; do
         base=$(basename "$dotfile")
         [ "$base" = "." ] || [ "$base" = ".." ] && continue
         target="$userdir/$base"
